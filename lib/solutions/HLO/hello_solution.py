0eb55e5f03cc6f479f086af29054c33c812d55fa
sbx_user1051 <sbx_user1051@169.254.69.249>
2026-01-13 17:02:46 +0000

    Tue Jan 13 17:02:46 UTC 2026

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,9 +3,6 @@ class HelloSolution:
     
     # friend_name = unicode string
     def hello(self, friend_name):
-       
-        # hello(string) -> string
-        #  - param[0] = a String containing a name
-        #  - @return = a String containing a message
-        # return "Hello, " + friend_name + "!"
-        return "Hello, World!"
+        return "Hello, " + friend_name + "!"
+
+
