7f481faa799fb009112a2a7c26efd922c7a4b17d
sbx_user1051 <sbx_user1051@169.254.69.249>
2026-01-13 16:56:00 +0000

    Tue Jan 13 16:56:00 UTC 2026

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,4 +2,8 @@
 class SumSolution:
     
     def compute(self, x, y):
-        raise NotImplementedError()
+        if(x < 0 or y < 0):
+            raise ValueError("Both numbers must be non-negative")
+        if(x >100 or y >100):
+            raise ValueError("Both numbers must be less than or equal to 100")
+        return x + y
