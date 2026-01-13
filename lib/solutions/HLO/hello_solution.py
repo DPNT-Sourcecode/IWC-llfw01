d8ed637e84a9a1943d5a44b12ff36251c2a3dbea
sbx_user1051 <sbx_user1051@169.254.69.249>
2026-01-13 17:01:46 +0000

    Tue Jan 13 17:01:46 UTC 2026

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -7,5 +7,5 @@ class HelloSolution:
         # hello(string) -> string
         #  - param[0] = a String containing a name
         #  - @return = a String containing a message
-        return "Hello, " + friend_name + "!"
-
+        # return "Hello, " + friend_name + "!"
+        return "Hello, World!"
