6b1d785d60cec6b71a3bcd6cc5c2918a7898a016
sbx_user1051 <sbx_user1051@169.254.69.249>
2026-01-13 17:14:28 +0000

    Tue Jan 13 17:14:28 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -9,3 +9,18 @@ def test_enqueue_size_dequeue_flow() -> None:
         call_size().expect(1),
         call_dequeue().expect("companies_house", 1),
     ])
+
+def test_rule_of_3() -> None: 
+    run_queue([
+        call_enqueue("provider_a", 42, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("provider_b", 43, iso_ts(delta_minutes=1)).expect(2),
+        call_enqueue("provider_a", 44, iso_ts(delta_minutes=2)).expect(3),
+        call_enqueue("provider_b", 44, iso_ts(delta_minutes=2)).expect(3),
+        call_enqueue("provider_c", 44, iso_ts(delta_minutes=2)).expect(3),
+        call_size().expect(3),
+        call_dequeue().expect("provider_c", 44),
+        call_dequeue().expect("provider_b", 44),
+        call_dequeue().expect("provider_a", 44),
+        call_dequeue().expect("provider_b", 43),
+        call_dequeue().expect("provider_a", 42),
+    ])
